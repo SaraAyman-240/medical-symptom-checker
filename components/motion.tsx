33c84f942e70ef6764c4fb533b7bde4013846068
SaraAyman-240 <sara.zeitoun@ejust.edu.eg>
2025-09-27 15:35:14 +0300

    Remove misplaced files to reorganize into frontend folder

--- components/motion.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-
-import { motion as framerMotion, AnimatePresence as framerAnimatePresence } from "framer-motion"
-
-export const motion = framerMotion
-export const AnimatePresence = framerAnimatePresence
